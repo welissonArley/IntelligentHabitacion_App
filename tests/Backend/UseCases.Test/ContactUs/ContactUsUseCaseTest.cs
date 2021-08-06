3ee284e537c8ef0983c69707c77434d62f90a778
welisson <welissonarleyvs@gmail.com>
2021-08-06 08:28:45 +0100

    Contact Us unit test for usecase

---
+++ tests/Backend/UseCases.Test/ContactUs/ContactUsUseCaseTest.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Homuai.Application.UseCases;
+using Homuai.Application.UseCases.ContactUs;
+using Homuai.Communication.Request;
+using System.Threading.Tasks;
+using Useful.ToTests.Builders.Entity;
+using Useful.ToTests.Builders.LoggedUser;
+using Useful.ToTests.Builders.Repositories;
+using Useful.ToTests.Builders.Services.Email;
+using Useful.ToTests.Builders.UseCaseCreateResponse;
+using Xunit;
+
+namespace UseCases.Test.ContactUs
+{
+    public class ContactUsUseCaseTest
+    {
+        [Fact]
+        public async Task Validade_Sucess_WithTrueResponse()
+        {
+            var useCase = CreateUseCase();
+
+            var validationResult = await useCase.Execute(new RequestContactUsJson
+            {
+                Message = "My Message"
+            });
+
+            validationResult.Should().BeOfType<ResponseOutput>();
+            validationResult.Token.Should().NotBeNullOrWhiteSpace();
+        }
+
+        private ContactUsUseCase CreateUseCase()
+        {
+            var user = UserBuilder.Instance().WithoutHomeAssociation();
+            var unitOfWork = UnitOfWorkBuilder.Instance().Build();
+            var homuaiUseCase = HomuaiUseCaseBuilder.Instance().Build();
+            var loggedUser = LoggedUserBuilder.Instance().User(user).Build();
+            var sendContactUsEmail = SendContactUsEmailBuilder.Instance().Build();
+
+            return new ContactUsUseCase(loggedUser, sendContactUsEmail, homuaiUseCase, unitOfWork);
+        }
+    }
+}
---
+++ tests/Backend/Useful.ToTests/Builders/Services/Email/SendContactUsEmailBuilder.cs
@@ -0,0 +1,30 @@
+﻿using Homuai.Domain.Services.SendEmail;
+using Moq;
+
+namespace Useful.ToTests.Builders.Services.Email
+{
+    public class SendContactUsEmailBuilder
+    {
+        private static SendContactUsEmailBuilder _instance;
+        private readonly Mock<ISendContactUsEmail> _repository;
+
+        private SendContactUsEmailBuilder()
+        {
+            if (_repository == null)
+            {
+                _repository = new Mock<ISendContactUsEmail>();
+            }
+        }
+
+        public static SendContactUsEmailBuilder Instance()
+        {
+            _instance = new SendContactUsEmailBuilder();
+            return _instance;
+        }
+
+        public ISendContactUsEmail Build()
+        {
+            return _repository.Object;
+        }
+    }
+}
